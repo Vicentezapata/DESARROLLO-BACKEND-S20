d276ae9a0525590c73923903a4edefd63174b393
Vicente Zapata <vzapata@ciisa.cl>
2025-04-02 20:18:31 -0300

    Carga de modelo de base de datos

---
+++ creacionDb.sql
@@ -0,0 +1,36 @@
+DROP TABLE IF EXISTS `ServiciosCiudades`;
+DROP TABLE IF EXISTS `Servicios`;
+DROP TABLE IF EXISTS `Ciudades`;
+DROP TABLE IF EXISTS `Nosotros`;
+
+CREATE TABLE `Nosotros`(
+    `id` BIGINT NOT NULL,
+    `Vision` VARCHAR(255) NOT NULL,
+    `Mision` VARCHAR(255) NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+CREATE TABLE `Servicios`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Nombre` VARCHAR(255) NOT NULL,
+    `Costo` BIGINT NOT NULL,
+    `Duracion` BIGINT NOT NULL,
+    `Tipo` BIGINT NOT NULL
+);
+
+CREATE TABLE `Ciudades`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Nombre` VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE `ServiciosCiudades`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `idServicio` BIGINT UNSIGNED NOT NULL,
+    `idCiudad` BIGINT UNSIGNED NOT NULL
+);
+
+ALTER TABLE
+    `ServiciosCiudades` ADD CONSTRAINT `serviciosciudades_idservicio_foreign` FOREIGN KEY(`idServicio`) REFERENCES `Servicios`(`id`);
+
+ALTER TABLE
+    `ServiciosCiudades` ADD CONSTRAINT `serviciosciudades_idciudad_foreign` FOREIGN KEY(`idCiudad`) REFERENCES `Ciudades`(`id`);
